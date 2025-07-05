9b47ba405438883a08f3acf538abfba7850b464d
eslamashraf10 <eslamashraf3645@gmail.com>
2025-07-05 07:49:45 +0300

    create.php

--- task2/create.php
+++ task2/create.php
@@ -2,12 +2,10 @@
 
 include 'db.php';
 
-// Sample data to insert
 $fname = 'eslam';
 $lname = 'ashraf';
 $age = 23;
 
-// SQL insert query
 $insert = $conn->query("INSERT INTO customer (fname, lname, age) VALUES ('$fname', '$lname', $age)");
 
 if ($insert) {
